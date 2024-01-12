703b6d618240444d675634821b33637cc8ce469d
teodosin <teodosin@protonmail.com>
2024-01-12 12:04:09 +0200

    Yeh

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { base } from '$app/paths';
+</script>
+
+<nav>
+	<a href="{base}/">Home</a>
+	<a href="{base}/about">About</a>
+</nav>
+
+<slot></slot>
+
--- src/routes/api/posts/+server.js
+++ src/routes/api/posts/+server.js
@@ -5,9 +5,9 @@ import { json } from '@sveltejs/kit';
 export const GET = async () => {
 	const allPosts = await fetchMarkdownPosts();
 
-	const sortedPosts = allPosts.sort((a, b) => {
-		return new Date(b.meta.date) - new Date(a.meta.date);
-	});
+	// const sortedPosts = allPosts.sort((a, b) => {
+	// 	return new Date(b.meta.date) - new Date(a.meta.date);
+	// });
 
-	return json(sortedPosts);
+	// return json(sortedPosts);
 };
