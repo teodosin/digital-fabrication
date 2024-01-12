d64e5c552d5f2c91750865ee53c774fc35918ae8
teodosin <teodosin@protonmail.com>
2024-01-12 11:54:23 +0200

    Pub?

--- src/routes/blog/[slug]/+page.js
+++ src/routes/blog/[slug]/+page.js
@@ -1,10 +1,13 @@
 // src/routes/blog/[slug]/+page.js
 
+// @ts-ignore
 export async function load({ params }) {
     try {
         const posts = import.meta.glob('/src/routes/blog/*.md');
         const post = await posts[`/src/routes/blog/${params.slug}.md`]();
+        // @ts-ignore
         const { title, date } = post.metadata;
+        // @ts-ignore
         const content = post.default;
         return {
             content,
