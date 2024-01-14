b167591aae456117352f8cf6b555242baec642ee
teodosin <teodosin@protonmail.com>
2024-01-14 12:11:04 +0200

    New try

--- src/lib/utils/onedrive-accessor.js
+++ src/lib/utils/onedrive-accessor.js
@@ -1,13 +1,22 @@
-const { Client } = require("@microsoft/microsoft-graph-client");
-const { TokenCredentialAuthenticationProvider } = require("@microsoft/microsoft-graph-client/authProviders/azureTokenCredentials");
-const fs = require('fs');
-const path = require('path');
+import { Client } from "@microsoft/microsoft-graph-client";
+import { TokenCredentialAuthenticationProvider } from "@microsoft/microsoft-graph-client/authProviders/azureTokenCredentials";
+import { ClientSecretCredential } from "@azure/identity";
+import fs from 'fs';
+import path from 'path';
 
-const { ClientSecretCredential } = require("@azure/identity");
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const tenantId = process.env.AZURE_TENANT_ID;
 const clientId = process.env.AZURE_CLIENT_ID;
 const clientSecret = process.env.AZURE_CLIENT_SECRET;
+
+if (!tenantId || !clientId || !clientSecret) {
+    throw new Error('Environment variables AZURE_TENANT_ID, AZURE_CLIENT_ID, or AZURE_CLIENT_SECRET are not set');
+}
 
 const tokenCredential = new ClientSecretCredential(tenantId, clientId, clientSecret);
 
@@ -29,6 +38,6 @@ async function syncFiles() {
       const fileContent = await client.api(`/me/drive/items/${driveItem.id}/content`).get();
       fs.writeFileSync(path.join(__dirname, 'src/posts', driveItem.name), fileContent);
     }
-  }
+}
 
 syncFiles();
